6f41b17b533bd3eb6ca3241b2e25c7294fb0d853
Julian Gonzalez <jgonzalezd.sist>
2013-08-29 09:55:34 -0500

    initial commit-basic styles

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ribbit::Application.config.secret_key_base = 'cc3470d7b020030772fa44e79f8da01c9110f91cd104355618be14759537cdd3dc614f9b6732ead1ac2309e72a55237b26e4b1d5187b78d1c275b91b5cf5a4b8'
