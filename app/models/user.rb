1cbe3071f2d1c9522dc966a7dabae9afe5195419
Julian Gonzalez <jgonzalezd.sist>
2013-08-30 00:19:42 -0500

    added soport for att_accesible

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,21 @@
 class User < ActiveRecord::Base
-  attr_accessible :avatar_url, :email, :name, :password, :password_confirmation
   has_secure_password
   
   before_validation :prep_email
+  before_save :create_avatar_url
   
   validates_presence_of :name
   validates :username, uniqueness: true, presence: true
-  validates :email, uniqueness: true, presence: true, format: { with: /^[\w.+-]+@([\w]+.)+\w+$/ }
+  validates :email, uniqueness: true, presence: true, format: { with: /\A[\w.+-]+@([\w]+.)+\w+\z/ }
   
   private
  
     def prep_email
         self.email = self.email.strip.downcase if self.email
+    end
+    
+    def create_avatar_url
+        self.avatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50"
     end
   
 end
