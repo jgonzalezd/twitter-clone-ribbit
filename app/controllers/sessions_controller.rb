2b900aa8b1e5b80ba1d8c845620f9b9b150f4d98
Julian Gonzalez <jgonzalezd.sist@gmail.com>
2013-08-31 10:59:42 -0500

    Added Sessions logic

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(username: params[:username].downcase)
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id #This is a security issue but for this purpose I will allow it.
+      redirect_to user, notice: "Logged in!" 
+    else
+      flash[:error] = "Incorrect User/Password combination"
+      redirect_to rooturl
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "You have logged out."
+  end
+end
